74a39d6575f0a78ce2393d84be4b8c7fe630e1d5
Louis Payne <louispayne10@hotmail.co.uk>
2022-09-15 01:07:51 +0100

    Add {read,write}_word memory functions for future instructions

--- src/memory.cpp
+++ src/memory.cpp
@@ -13,6 +13,17 @@ void Memory::write_byte(uint16_t addr, uint8_t data)
     m_Memory[addr] = data;
 }
 
-// uint16_t Memory::read_word(uint16_t addr) const { NOT_IMPLEMENTED(); }
+uint16_t Memory::read_word(uint16_t addr) const
+{
+    const uint8_t hi_byte = m_Memory[addr];
+    const uint8_t lo_byte = m_Memory[addr + 1];
+    return (uint16_t)((hi_byte << 8U) | lo_byte);
+}
 
-// void Memory::write_word(uint16_t addr, uint16_t data) { NOT_IMPLEMENTED(); }
+void Memory::write_word(uint16_t addr, uint16_t data)
+{
+    const uint8_t hi_byte = (uint8_t)((data & 0xFF00U) >> 8U);
+    const uint8_t lo_byte = data & 0xFFU;
+    m_Memory[addr]        = hi_byte;
+    m_Memory[addr + 1]    = lo_byte;
+}
--- src/memory.h
+++ src/memory.h
@@ -8,8 +8,8 @@ class Memory
 public:
     uint8_t read_byte(uint16_t addr) const;
     void write_byte(uint16_t addr, uint8_t data);
-    // uint16_t read_word(uint16_t addr) const;
-    // void write_word(uint16_t addr, uint16_t data);
+    uint16_t read_word(uint16_t addr) const;
+    void write_word(uint16_t addr, uint16_t data);
 
 private:
     std::array<uint8_t, 0xFFFF> m_Memory = {};
